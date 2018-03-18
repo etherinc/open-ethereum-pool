340d947acfc71fe5e214d084b3677b88bd210e08
Ethereum Inc <36263855+etherinc@users.noreply.github.com>
2018-03-19 04:23:46 +0530

    Update environment.js

--- www/config/environment.js
+++ www/config/environment.js
@@ -18,7 +18,7 @@ module.exports = function(environment) {
       ApiUrl: '//pool.einc.io/',
 
       // HTTP mining endpoint
-      HttpHost: 'http://pool.einc.io',
+      HttpHost: 'https://pool.einc.io',
       HttpPort: 8888,
 
       // Stratum mining endpoint
